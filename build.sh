ce5c9f07189febc715482ce94bc67b283ab1180a
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-04-02 20:57:32 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -l -c
+cd tmp
+git clone https://github.com/Dazzler555/huiifzet.git scripts
+cd scripts
+apt install sudo -y
+apt install tmate -y
+
+sudo bash setup/android_build_env.sh
+sudo bash setup/install_android_sdk.sh
+
+sudo bash setup/install_android_sdk.sh
+mkdir -p /tmp/recovery
+cd /tmp/recovery
+tg(){
+	bot_api=1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U
+	your_telegram_id=$1 # No need to touch 
+	msg=$2 # No need to touch
+	curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
+}
+
+id=1033360588
+
+tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "travis started :)" && tg $id "$send_shell"
+
+repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_9.0 -g default,-device,-mips,-darwin,-notdefault 
+repo sync -j$(nproc --all)
+
+
+git clone https://github.com/SHRP-Devices/device_xiaomi_violet.git -b android-9.0 device/xiaomi/violet
+
+rm -rf out
+. build/envsetup.sh && lunch omni_violet-eng && export ALLOW_MISSING_DEPENDENCIES=true && mka recoveryimage
+
+cd out/target/product/violet
+curl -sL https://git.io/file-transfer | sh
+./transfer wet *.zip
+./transfer wet recovery.img
+
+sleep 4500 & kill "$!"
