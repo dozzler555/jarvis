0ddd87d3a878bea6d2c59d35a90792a4f0831f54
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-04-02 12:12:48 -0400

    trial-1

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
 #!/bin/sh -l -c
 cd tmp
 ls
-git clone https://github.com/Dazzler555/huiifzet.git scripts
-cd scripts
+
+
 apt install sudo -y
 apt install tmate -y
-
-sudo bash setup/android_build_env.sh
+apt install -y git
+apt install adb -y
+apt install automake -y
+apt install \
+    adb autoconf automake axel bc bison build-essential \
+    ccache clang cmake expat fastboot flex g++ \
+    g++-multilib gawk gcc gcc-multilib git gnupg gperf \
+    htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
+    libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
+    libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
+    maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
+    pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
+    texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
+    libxml-simple-perl apt-utils -y
 
 
 
