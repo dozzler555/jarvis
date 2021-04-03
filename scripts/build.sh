1f3ea264e60448f975b559cce7bd9aaf2c330ec0
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-04-03 10:32:47 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 sudo -E apt-get -qq install bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev repo rsync flex curl  bison aria2
-
+apt install git
 # sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
 
 # sudo chmod a+rx /usr/local/bin/repo
 
 mkdir -p /tmp/recovery
 cd /tmp/recovery
-tg(){
-	bot_api=1744981054:AAEwTewZaL8Z6K49crBWlfRnW3Zi9Aqim6U
-	your_telegram_id=$1 # No need to touch 
-	msg=$2 # No need to touch
-	curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" --data "text=$msg&chat_id=${your_telegram_id}"
-}
-
-id=1033360588
-
-tmate -S /tmp/tmate.sock new-session -d && tmate -S /tmp/tmate.sock wait tmate-ready && send_shell=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}') && tg $id "travis started :)" && tg $id "$send_shell"
 
 repo init --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_9.0 -g default,-device,-mips,-darwin,-notdefault 
 repo sync -j$(nproc --all)
